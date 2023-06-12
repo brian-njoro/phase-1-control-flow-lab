f8da1edb7005ed775ea242df7c90a2b0a5fbcc14
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-06-12 21:15:38 +0300

    add respective changes to index.js

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let feet=199
+function scuberGreetingForFeet(feet){ 
+  if (feet <= 400) {
+    return `This one is on me`
+  } else if (feet >= 2000) {
+    return `I will gladly take your thirty bucks`
+  } else if (feet >= 2500) {
+    return `no can do!`
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let city = `NYC`
+function ternaryCheckCity(city){
+  return ( city=`NYC` ? `OK Sounds good` : `No Go !`)
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tip=`generous`
+let gratitude
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case `generous` :
+      gratitude = `Thank you so much`
+      break
+      case `not generous` :
+        gratitude = `Thank you`
+        default:
+          gratitude = `Thanks for everything. Bye!`
+
+  }
 }
+
+console.log (ternaryCheckCity(`NYC`))
+console.log(scuberGreetingForFeet(2000))
+console.log(switchOnCharmFromTip(`generous`))
